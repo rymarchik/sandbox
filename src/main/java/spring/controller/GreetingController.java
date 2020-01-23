19e4eff3f76f7977b70fea036312814f719f9648
rymarchikas <arymarchik@qulix.com>
2020-01-23 07:39:01 +0300

    RESTful web-service

---
+++ src/main/java/spring/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package spring.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import spring.entity.Greeting;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
