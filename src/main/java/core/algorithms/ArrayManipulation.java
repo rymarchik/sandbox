23d06658fa6715174717bbc159830bc361d3948e
rymarchik <insurg265@gmail.com>
2025-12-05 04:29:18 +0100

    ArrayManipulation

---
+++ src/main/java/core/algorithms/ArrayManipulation.java
@@ -0,0 +1,52 @@
+package core.algorithms;
+
+import java.util.Arrays;
+
+/**
+ * Given an array a, your task is to output an array b of the same length by applying the following transformation:
+ * – For each i from 0 to a.length - 1 inclusive, b[i] = a[i - 1] + a[i] + a[i + 1]
+ * – If an element in the sum a[i - 1] + a[i] + a[i + 1] does not exist, use 0 in its place
+ * – For instance, b[0] = 0 + a[0] + a[1]
+ */
+public class ArrayManipulation {
+
+    private static int[] process(int[] a) {
+        int n = a.length;
+        int[] r = new int[n];
+
+        if (n == 0) return r;
+
+        if (n == 1) {
+            r[0] = a[0];
+            return r;
+        }
+
+        // левый край
+        r[0] = a[0] + a[1];
+
+        // середина
+        for (int i = 1; i < n - 1; i++) {
+            r[i] = a[i - 1] + a[i] + a[i + 1];
+        }
+
+        // правый край
+        r[n - 1] = a[n - 2] + a[n - 1];
+
+        return r;
+    }
+
+    public static void testCase1() {
+        int[] array = new int[]{4, 0, 1, -2, 3};
+        System.out.println(Arrays.toString(process(array)));
+    }
+
+    public static void testCase2() {
+        int[] array = new int[]{1};
+        System.out.println(Arrays.toString(process(array)));
+    }
+
+    public static void testCase3() {
+        int[] array = new int[]{};
+        System.out.println(Arrays.toString(process(array)));
+    }
+}
